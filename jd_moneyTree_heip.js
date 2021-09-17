c68d36c6c8bda0af9e1b4df53f3f03d6c40e24f0
xpqdudu <31915581+xpqdudu@users.noreply.github.com>
2021-09-17 09:29:36 +0800

    Update jd_moneyTree_heip.js

--- jd_moneyTree_heip.js
+++ jd_moneyTree_heip.js
@@ -221,9 +221,8 @@ function shareCodesFormat() {
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将为本脚本作者【zero205】助力\n`)
-      const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
+      console.log(`由于您第${$.index}个京东账号未提供shareCode, 请运行code.sh\n`)
+
     }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
